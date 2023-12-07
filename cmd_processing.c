0c1e3679f28e24437afc141213e1bfde465abd11
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-12-07 11:11:44 -0800

    made series of changes

--- cmd_processing.c
+++ cmd_processing.c
@@ -1,8 +1,11 @@
 #include "s_sh.h"
+
+
 void process(void)
 {
 	pid_t pid;
 	int val;
+	int fd[2];
 	
 	char *argv[] = {"/bin/ls", NULL};
 	pid = fork();
@@ -13,10 +16,16 @@ void process(void)
 	}
 	if (pid == 0)
 	{
+		pipe(fd);
+
+		dup2(fd[1], STDOUT_FILENO);
+
+		close(fd[0]);
 		val = execve(argv[0], argv, NULL);
 		if (val == -1)
 		{
 			perror("error");
+			exit(EXIT_FAILURE);
 		}
 	}
 	else
