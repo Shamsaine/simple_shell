1c6a6da430cadd9d63ec5d46596c4d89a1d05fc6
Shamsudeen Ibrahim <shamsain3@gmail.com>
2023-12-09 13:24:21 -0800

    made adjustment to the cmd_processing file

--- cmd_processing.c
+++ cmd_processing.c
@@ -1,7 +1,38 @@
 #include "s_sh.h"
 
+void process(char *args[]) {
+    pid_t pid;
+    int status;
+    int fd[2];
 
-void process(void)
+    pipe(fd);
+
+    pid = fork();
+
+    if (pid == 0) {
+        /* Child process */
+        close(fd[0]); /* Close the read end of the pipe */
+        dup2(fd[1], STDOUT_FILENO); /* Redirect stdout to the write end of the pipe*/
+        close(fd[1]); /* Close the original stdout descriptor */
+        execve(args[0], args, NULL);
+        perror("Error executing command");
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        /* Forking error */
+        perror("Error forking");
+    } else {
+        /* Parent process */
+        close(fd[1]); /* Close the write end of the pipe */
+        waitpid(pid, &status, 0); /* Wait for the child process to finish */
+        /* Read and handle output from the pipe if needed */
+        close(fd[0]); /* Close the read end of the pipe */
+    }
+}
+
+
+
+
+/*void process(void)
 {
 	pid_t pid;
 	int val;
@@ -34,4 +65,4 @@ void process(void)
 		printf("done with execve\n");
 	}
 	return;
-}
+}*/
