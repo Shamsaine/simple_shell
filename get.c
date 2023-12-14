5a21f7e9d9fb3c9ca965e37bd50ce989aba47586
MOH2056 <tapereyetunde@gmail.com>
2023-12-13 17:56:20 -0800

    message

--- get.c
+++ get.c
@@ -7,7 +7,7 @@
 char *pathfinder(const char *userinput)
 {
 	char *path_environ = getenv("PATH");
-	char *way = my_strtok(path_environ, ":");
+	char *way = strtok(path_environ, ":");
 	char *fullypath;
 
 	while (way != NULL)
@@ -24,7 +24,7 @@ char *pathfinder(const char *userinput)
 			return (fullypath);
 		}
 		free(fullypath);
-		way = my_strtok(NULL, ":");
+		way = strtok(NULL, ":");
 	}
 	return (NULL);
 }
